c1f94e9777685218f7f89f8001af1b4f9ff60657
joris-rotteveel <joris.rotteveel@touchtech.co.nz>
2016-04-23 18:48:53 +1200

    ADDED: switch between principals

--- src/scripts/mvc/view/pages/principals.js
+++ src/scripts/mvc/view/pages/principals.js
@@ -18,18 +18,40 @@ module.exports = BaseView.extend({
 
     onNameClick: function (e) {
         this.$el.addClass('person-selected');
-        var $name = $(e.target);
+        var $name = $(e.currentTarget);
         var data = $name.data();
         var person = '.' + data.person;
         this.$('span').addClass('hidden');
-        this.$('span' + person).removeClass('hidden');
+        this.$('.js-names').addClass('hidden');
+        this.$('.js-mugshot').addClass(data.person)
 
+        this.$('span' + person).removeClass('hidden');
+        this.$('.mugshot').removeClass('hidden');
+        this.$('.js-title').removeClass('pink');
+
+        this.$el.on('mousedown', _.bind(this.onExitClick, this));
+    },
+
+    onExitClick: function () {
+
+        console.log('yes')
+        this.$el.removeClass('person-selected');
+        this.$('.js-names').removeClass('hidden');
+        this.$('span').removeClass('hidden');
+
+        this.$('.js-mugshot').removeClass('matt josh scott brad abe');
+
+        this.$('js-info-holder span').addClass('hidden');
+        this.$('.mugshot').addClass('hidden');
+        this.$('.js-title').addClass('pink');
+
+        this.$el.off('mousedown');
 
     },
 
     onResize: function () {
         var padding = 300;
-        var textHeight = this.$('.js-copy').height() + padding -this.$('.js-info-holder').height();
+        var textHeight = this.$('.js-copy').height() + padding - this.$('.js-info-holder').height();
         var totalHeight = this.$el.height();
         var offset = totalHeight / 2 - textHeight / 2;
 
