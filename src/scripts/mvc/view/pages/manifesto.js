1f51be359ce5adf6cb122e1dff6a247f09ecc58b
joris-rotteveel <joris.rotteveel@touchtech.co.nz>
2016-05-04 21:43:00 +1200

    ADDED: positioning of manifesto and show/hide animation :D

--- src/scripts/mvc/view/pages/manifesto.js
+++ src/scripts/mvc/view/pages/manifesto.js
@@ -9,18 +9,58 @@ module.exports = BaseView.extend({
 
     initialize: function (options) {
         BaseView.prototype.initialize.apply(this);
+        this.title = this.$('.js-title');
+        this.copy = this.$('.js-copy');
+
+        TweenMax.set(this.title,{alpha:0});
+        TweenMax.set(this.copy,{alpha:0});
     },
 
     render: function () {
-        this.$el.on('click', _.bind(this.onClick, this));
     },
 
-    onClick: function () {
-        EventBus.trigger(EventBus.EVENTS.NAVIGATE, AppModel.PAGES.CLIENTS);
+    show: function () {
+
+        var delay = 0;
+        var time = 0.5;
+        TweenMax.killTweensOf(this.title);
+        TweenMax.killTweensOf(this.copy);
+
+        var startProps = {
+            alpha: 0,
+            y    : -10
+        };
+
+        TweenMax.fromTo(this.title, time,
+                        startProps,
+                        {
+                            delay: delay,
+                            alpha: 1,
+                            y    : 0
+                        });
+
+        TweenMax.fromTo(this.copy, time,
+                        startProps,
+                        {
+                            delay: delay + time,
+                            alpha: 1,
+                            y    : 0
+                        });
+
+    },
+
+    hide: function () {
+        TweenMax.killTweensOf(this.title);
+        TweenMax.killTweensOf(this.copy);
+
+        var time = 0.5;
+
+        TweenMax.to(this.title, time, {alpha: 0});
+        TweenMax.to(this.copy, time, {alpha: 0});
+
     },
 
     destroy: function () {
-        this.$el.off();
         BaseView.prototype.destroy.apply(this);
     },
 
