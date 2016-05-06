e76416f18520028586016b74418282ca8cb6fcfb
joris-rotteveel <joris.rotteveel@touchtech.co.nz>
2016-05-06 20:50:23 +1200

    ADDED: WIP show animation Client logos

---
+++ src/scripts/mvc/view/modules/idol_element.js
@@ -0,0 +1,40 @@
+var BaseView = require('../base/base_view');
+var TweenMax = require('TweenMax');
+var Config = require('Config');
+var $ = require('jquery');
+var _ = require('underscore');
+
+module.exports = BaseView.extend({
+
+    initialize: function (options) {
+        BaseView.prototype.initialize.apply(this);
+
+        this.originalY = this.$el.offset().top;
+        TweenMax.set(this.$el, {
+            y    : 100,
+            alpha: 0
+        });
+
+    },
+
+    show: function (delay) {
+
+        if (!this.isShowing) {
+            this.isShowing = true;
+            TweenMax.to(this.$el, 0.2, {
+                delay: delay,
+                y    : 0,
+                alpha: 1
+            });
+        }
+
+    },
+
+    setStartPosition: function () {
+        if (!this.isShowing) {
+            this.originalY = this.$el.offset().top;
+        }
+    }
+
+});
+
