cebdf0ce1364335c3b8b6af8c1e38dca83252281
joris-rotteveel <joris.rotteveel@touchtech.co.nz>
2016-05-18 22:36:13 +1200

    ADDED: show mobile menu only on correct screen sizes

--- src/scripts/mvc/view/modules/mobile_menu.js
+++ src/scripts/mvc/view/modules/mobile_menu.js
@@ -15,6 +15,9 @@ module.exports = BaseView.extend({
         this.$('.js-hamburger').on('click', _.bind(this.onClick, this));
         this.html = $('html');
 
+        TweenMax.set(this.$('.js-menu'), {autoAlpha: 0});
+        TweenMax.set(this.$('li'), {alpha: 0});
+
     },
 
     setBlack: function () {
@@ -28,11 +31,46 @@ module.exports = BaseView.extend({
     },
 
     onClick: function (e) {
+        if (this.$('.js-hamburger').hasClass('is-active')) {
+
+            this.hide();
+        } else {
+            this.show();
+        }
+
+    },
+
+    show: function () {
         this.setWhite();
-        this.$el.toggleClass('full');
-        this.$('.js-hamburger').toggleClass('is-active');
-        this.$('.js-menu').toggleClass('is-active');
-        this.html.toggleClass('no-scroll');
+        this.$el.addClass('full');
+        this.$('.js-hamburger').addClass('is-active');
+        this.html.addClass('no-scroll');
+        TweenMax.to(this.$('.js-menu'), 0.1, {autoAlpha: 1});
+        var delay = 0;
+        _.each(this.$('li'), function (li) {
+            TweenMax.to(li, 0.2, {
+                delay: delay,
+                alpha: 1,
+                x    : 0
+            });
+            delay += 0.05;
+        }, this);
+    },
+    hide: function () {
+        TweenMax.to(this.$('.js-menu'), 0.1, {autoAlpha: 0});
+        TweenMax.to(this.$('li'), 0.1, {
+            delay: 0,
+            alpha: 0
+        });
+        this.determineColor();
+        this.$el.removeClass('full');
+        this.$('.js-hamburger').removeClass('is-active');
+        this.html.removeClass('no-scroll');
+    },
+
+    determineColor: function () {
+
+        this.setBlack();
     }
 
 });
